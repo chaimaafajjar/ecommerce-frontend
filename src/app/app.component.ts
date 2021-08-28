e25650281cf7359a86ffee19c0f9981b9666d0f2
chaimaafajjar <ch.fajjar99@gmail.com>
2021-08-28 22:00:04 +0200

    version 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CatalogueService} from "./catalogue.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,8 @@ import {CatalogueService} from "./catalogue.service";
 })
 export class AppComponent implements OnInit{
 public categories;
-constructor(private catService:CatalogueService){}
+public currentCategorie;
+constructor(private catService:CatalogueService , private router:Router ){}
 
 ngOnInit():void{
 this.getCategories();
@@ -23,5 +25,15 @@ console.log(err);
 })
 
 }
+
+  getProductsByCat(c){
+  this.currentCategorie=c;
+this.router.navigateByUrl('/products/2/'+c.id);
+  }
+
+  onSelectedProducts(){
+  this.currentCategorie=undefined;
+  this.router.navigateByUrl("/products/1/0");
+  }
 
 }
